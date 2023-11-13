aadc5de7dd4887658b835c53300acfe1681ded67
Josh LaBounty <jjlab@uw.edu>
2023-11-13 06:17:29 +0100

    Fixes typo in script

--- scripts/rsync_data.sh
+++ scripts/rsync_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DAQ_BASE_DIR = '/home/pioneer/pioneer/pioneer-teststand/nearline'
-DAQ_LOG_DIR = '/home/pioneer/pioneer/pioneer-teststand/nearline_logs'
+DAQ_BASE_DIR="/home/pioneer/pioneer/pioneer-teststand/nearline"
+DAQ_LOG_DIR="/home/pioneer/pioneer/pioneer-teststand/nearline_logs"
 
-BASE_DIR = '/home/pioneer/dqm/nearline'
-LOG_DIR = '/home/pioneer/dqm/nearline_logs'
+BASE_DIR="/home/pioneer/dqm/nearline/"
+LOG_DIR="/home/pioneer/dqm/nearline_logs/"
 
 time rsync -avh j.carlton@daq:$DAQ_BASE_DIR $BASE_DIR
 time rsync -avh j.carlton@daq:$DAQ_LOG_DIR $LOG_DIR
